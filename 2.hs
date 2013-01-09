fb83826463ba4dba6c47d7b5aa04f55a668b619f
Matt Bray <mattjbray@gmail.com>
2013-01-09 16:31:31 +0000

    Problem 2: Use putStrLn

--- 2.hs
+++ 2.hs
@@ -4,5 +4,5 @@ import System.IO
 main = do
   handle <- openFile "2.data" ReadMode
   contents <- hGetContents handle
-  putStr $ filter isLetter contents
+  putStrLn $ filter isLetter contents
   hClose handle
