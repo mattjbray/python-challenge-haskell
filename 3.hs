ea38f23b86045595c0a0852a1579408d50160e1c
Matt Bray <mattjbray@gmail.com>
2013-01-10 13:02:16 +0000

    Problem 3: Refactor

--- 3.hs
+++ 3.hs
@@ -4,10 +4,14 @@ import System.IO
 main = do
   handle <- openFile "3.data" ReadMode
   contents <- hGetContents handle
-  putStrLn $ show $ doStuff contents
+  putStrLn $ (concatSubMatches . getMatches) contents
   hClose handle
 
-doStuff contents = concat $ map f ( doMatch contents)
+-- extracts submatches from a list of [match, submatch] and concatenates them
+concatSubMatches :: [[String]] -> String
+concatSubMatches = concatMap f
   where f [_,x] = x
 
-doMatch x = x =~ "[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]" :: [[String]]
+-- returns a list of [match, submatch]
+getMatches :: String -> [[String]]
+getMatches x = x =~ "[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]"
